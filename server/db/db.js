068cc78b7fabc2c49bc4d1643086e952006a4466
Meet Patel <patel00446@gmail.com>
2022-02-16 12:56:39 -0500

    Setup done

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,28 @@
 const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const username = "postgres";
+const host = "localhost";
+const dialectName = "postgres";
+const database = "messenger";
+const password = "meetpatel";
 
+const db = new Sequelize(
+  database,
+  username,
+  password || "postgres://localhost:5432/messenger",
+  {
+    host,
+    password,
+    dialectName,
+    ssl: "meetpatel",
+  }
+);
+console.log(db);
+try {
+  Sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:");
+}
 module.exports = db;
